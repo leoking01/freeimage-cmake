92bb00d9ef955689ecdcfe84dc2a9e240f719587
leo <leoking02@foxmail.com>
2022-10-11 16:05:02 +0800

    freeimage_cmk_suppt_inst_Lite   add  external  app  .

---
+++ src_extern_app/mymathApp.cc
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "FreeImage.h"
+int main(int argc, char const *argv[])
+{
+    const char * freeimageVer  = FreeImage_GetVersion( ) ;
+    std::  cout << "freeimageVer = " << freeimageVer <<std:: endl;
+    return 0;
+}
